09cba8c453aafb845062f58d5bfefd01fc6d5812
Eugene Che <indexpro24@gmail.com>
2024-03-10 16:25:56 +0300

    c

--- project/src/app/(production)/catalog/[slug]/page.jsx
+++ project/src/app/(production)/catalog/[slug]/page.jsx
@@ -1,18 +1,30 @@
 import { HeadProduct, ProductList } from "@/UI";
 import { getProduct, getOneCategory } from "../server";
 
-export const metadata = {
-  title: "Фабрика ЗОВ | Кухонные гарнитуры",
-  description: "Кухонные гарнитуры от фабрики ЗОВ",
-};
+// export const metadata = {
+//   title: "Фабрика ЗОВ | Кухонные гарнитуры",
+//   description: "Кухонные гарнитуры от фабрики ЗОВ",
+// };
 
-const data1 = {
-  head: {
-    h1: "Кухонные гарнитуры",
-    description:
-      "Выберите понравившуюся вам конфигурацию, материал, стиль и цвет кухонного гарнитура",
-  },
-};
+// const data1 = {
+//   head: {
+//     h1: "Кухонные гарнитуры",
+//     description:
+//       "Выберите понравившуюся вам конфигурацию, материал, стиль и цвет кухонного гарнитура",
+//   },
+// };
+
+
+export async function generateMetadata({ params }) {
+	const meta = await getOneCategory({ params })
+
+	return {
+		title:
+			meta.category_one.value,
+		description: meta.category_one.value,
+			
+	}
+}
 
 export default async ({params}) => {
 //   const products = await getProduct();
