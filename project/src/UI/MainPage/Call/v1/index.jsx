e17f7b49da448fd13cc1b8bd0e6ff1ff736ab211
Eugene Che <indexpro24@gmail.com>
2024-03-11 07:14:03 +0300

    с

--- project/src/UI/MainPage/Call/v1/index.jsx
+++ project/src/UI/MainPage/Call/v1/index.jsx
@@ -13,17 +13,18 @@ export default () => {
         <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
           <div className="mx-auto max-w-screen-lg text-center font-display">
             <h2 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-6xl">
-              Готовы к покупке мебели?
+              Хотите узнать цену?
             </h2>
-            <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
+            {/* <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
               Подайте заявку на бесплатный дизайн проект.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 ">
+            </p> */}
+            <div className="mt-10 flex items-center justify-center  ">
               <button
-                onClick={openVisibleFormConsultation}
-                className="rounded-md bg-gray-900 px-8 py-2.5 text-sm font-semibold shadow-xl shadow-gray-500 text-white hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={openVisibleFormConsultation}               
+                className="block  rounded-md  px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 
+                animate-background-shine items-center justify-center  border border-red-800 bg-[linear-gradient(110deg,#b91c1c,45%,#dc2626,55%,#b91c1c)] bg-[length:200%_100%]  transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
               >
-                Бесплатный дизайн проект
+                Закажите бесплатный дизайн проект
               </button>
             </div>
           </div>
