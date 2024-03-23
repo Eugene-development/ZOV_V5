31f05e86b8c5b96859ea9ac2dd416bd9c9a624d3
Eugene Che <indexpro24@gmail.com>
2024-03-23 23:35:36 +0300

    c

--- project/src/app/(production)/catalog/[slug]/page.jsx
+++ project/src/app/(production)/catalog/[slug]/page.jsx
@@ -15,7 +15,7 @@ export async function generateMetadata({ params }) {
 export default async ({ params }) => {
   const data = await getOneCategory({ params });
 
-  const stats = [
+  const breadCrumbs = [
     { name: 'Рубрика', value: 'Мебель' },
     { name: 'Категория', value: data.category_one.value, count: data.category_one.product.length, unit: 'поз.' },
   ]
@@ -33,12 +33,12 @@ export default async ({ params }) => {
       <div className="bg-gray-900">
         <div className="max-w-screen-2xl px-4 mx-auto sm:px-6">
           <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 ">
-            {stats.map((stat) => (
-              <div key={stat.name} className="bg-gray-900 px-4 py-5 sm:px-6 lg:px-8">
-                <p className="text-sm font-medium leading-6 text-gray-400">{stat.name}</p>
+            {breadCrumbs.map((item) => (
+              <div key={item.name} className="bg-gray-900 px-4 py-5 sm:px-6 lg:px-8">
+                <p className="text-sm font-medium leading-6 text-gray-400">{item.name}</p>
                 <p className="mt-2 flex items-baseline gap-x-2">
-                  <span className="text-3xl font-semibold tracking-tight text-white">{stat.value}</span>
-                  {stat.count ? <span className="text-sm text-gray-400">({stat.count} {stat.unit})</span> : null}
+                  <span className="text-3xl font-semibold tracking-tight text-white">{item.value}</span>
+                  {item.count ? <span className="text-sm text-gray-400">({item.count} {item.unit})</span> : null}
                 </p>
               </div>
             ))}
