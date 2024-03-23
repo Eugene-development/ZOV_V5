7e775378b8768069558ee92633b47626de003aa4
Eugene Che <indexpro24@gmail.com>
2024-03-23 22:55:16 +0300

    с

--- project/src/UI/MainPage/Colors/v1/index.jsx
+++ project/src/UI/MainPage/Colors/v1/index.jsx
@@ -51,24 +51,24 @@ export default () => {
             <div className="pointer-events-none relative flex gap-6 ">
               <div className=" flex  min-w-full shrink-0 items-center justify-around gap-6">
                 <img
-                  className="rotate-[3deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-[#c2b098]"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-[#c2b098]"
                   src="https://zovrus.ru/files/_cache/product/chr-5650.resize1200x1200w.jpg?99512fcbd6b465ce180887a22497881a"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[-2deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-green-800"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-green-800"
                   src="https://zovrus.ru/files/_cache/product/dsc08417.resize1200x1200w.jpg?ec23029eadf7df31e3e86bf91a6a148c"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[-5deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-pink-900"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-pink-900"
                   src="https://zovrus.ru/files/_cache/product/chr-2152.resize1200x1200w.jpg?f3b82632ecac9231dab2f9442233cec9"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[2deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-yellow-600"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-yellow-600"
                   src="https://zovrus.ru/files/_cache/product/chr-8945.resize800x800w.jpg?d1734f6452cf2d662162fce1a1ea377f"
-                  alt=""
+                  alt="цвет кухни"
                 />
               </div>
             </div>
@@ -76,24 +76,24 @@ export default () => {
             <div className="mt-16 pointer-events-none relative flex gap-6">
               <div className=" flex min-w-full shrink-0 items-center justify-around gap-6 ">
                 <img
-                  className="rotate-[2deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-[#5a5a74] "
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-[#5a5a74] "
                   src="https://zovrus.ru/files/_cache/product/chr-3018.resize800x800w.jpg?e74c691be26de5528a034a590850de05"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[-3deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-lime-800"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-lime-800"
                   src="https://zovrus.ru/files/_cache/product/dsc08929.resize1200x1200w.jpg?a273c3827b30640f85991bf1f23dc707"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[-2deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-blue-400"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-blue-400"
                   src="https://zovrus.ru/files/_cache/product/chr-0670_2.resize800x800w.jpg?e203baad1ef1f66dffc7d65195163458"
-                  alt=""
+                  alt="цвет кухни"
                 />
                 <img
-                  className="rotate-[1deg] aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-gray-800"
+                  className="aspect-square max-w-[clamp(22rem,28vmin,22rem)] rounded-md object-cover shadow-2xl shadow-gray-800"
                   src="https://zovrus.ru/files/_cache/product/chr-3780.resize1200x1200w.jpg?8edcdce1cd9a796b5c426d3c25905ccf"
-                  alt=""
+                  alt="цвет кухни"
                 />
               </div>
             </div>
