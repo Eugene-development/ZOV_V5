46e6ebfe21396610ff457f63ee3e4d5c74141781
Eugene Che <indexpro24@gmail.com>
2024-03-24 17:17:30 +0300

    c

--- project/src/UI/CatalogPage/Similar/v1/index.jsx
+++ project/src/UI/CatalogPage/Similar/v1/index.jsx
@@ -1,58 +1,60 @@
 'use client'
-
+import Link from "next/link";
 import { useProductStore } from "@/store/product";
 const { similarProducts } = useProductStore;
 
-const products = [
-    {
-        id: 1,
-        name: 'Fusion',
-        category: 'UI Kit',
-        href: '#',
-        price: '$49',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-01.jpg',
-        imageAlt:
-            'Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.',
-    },
-    // More products...
-]
 
 export default () => {
     const { currentSimilarProducts } = similarProducts();
-    console.log(currentSimilarProducts)
+
+    function getRandomElements(arr, numElements) {
+        // Создаем копию исходного массива
+        let arrCopy = arr.slice();
+
+        // Перемешиваем массив
+        for (let i = arrCopy.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]]; // Это ES6 деструктуризация для обмена элементами
+        }
+
+        // Возвращаем первые numElements элементов из перемешанного массива
+        return arrCopy.slice(0, numElements);
+    }
+    const fourRandomProducts = getRandomElements(currentSimilarProducts, 4);
+
+    console.log(fourRandomProducts)
 
     return (
         <div className="bg-gray-50">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                {/* <div className="flex items-center justify-between space-x-4">
-                    <h2 className="text-lg font-medium text-gray-900">Customers also viewed</h2>
-                    <a href="#" className="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                        View all
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-20 lg:max-w-full lg:px-24">
+                <div className="flex items-center justify-between space-x-4">
+                    <h2 className="text-lg font-medium text-gray-900">Похожие из категории</h2>
+                    <a href="#" className="whitespace-nowrap text-sm font-medium text-red-700 hover:text-red-600">
+                        Показать все из категории
                         <span aria-hidden="true"> &rarr;</span>
                     </a>
-                </div> */}
+                </div>
                 <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
+                    {fourRandomProducts.map((product) => (
+                        <Link key={product.slug} href={`/product/${product.slug}`} className="group relative">
                             <div className="aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
-                                <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center" />
+                                <img src={`${process.env.NEXT_PUBLIC_S3}/${product?.image[0]?.hash}`} alt={product.value} className="object-cover object-center" />
                                 <div className="flex items-end p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
                                     <div className="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">
-                                        View Product
+                                        Проект - {product.value}
                                     </div>
                                 </div>
                             </div>
-                            <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
+                            {/* <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
                                 <h3>
-                                    <a href="#">
+                                    <Link href={`/product/${product.slug}`}>
                                         <span aria-hidden="true" className="absolute inset-0" />
-                                        {product.name}
-                                    </a>
+                                        Проект - {product.value}
+                                    </Link>
                                 </h3>
-                                <p>{product.price}</p>
-                            </div>
-                            <p className="mt-1 text-sm text-gray-500">{product.category}</p>
-                        </div>
+                            </div> */}
+                            {/* <p className="mt-1 text-sm text-gray-500">{product.category}</p> */}
+                        </Link>
                     ))}
                 </div>
             </div>
