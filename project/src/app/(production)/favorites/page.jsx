d646c2a73445e6d300a96c953a50a07894c5342a
Eugene Che <indexpro24@gmail.com>
2024-03-29 19:23:06 +0300

    c

--- project/src/app/(production)/favorites/page.jsx
+++ project/src/app/(production)/favorites/page.jsx
@@ -1,6 +1,40 @@
+'use client'
+import { useState, useEffect } from 'react'
+
+import { BreadCrumbs, ProductList, HeadProductList } from "@/UI";
+
+import { useProductStore } from "@/store/product";
+const { favoritesProducts } = useProductStore;
+
+const breadCrumbs = [
+    { name: 'Раздел', value: 'Каталог' },
+    { name: 'Категория', value: 'ererer', count: '333', unit: 'поз.' },
+]
+
+// export const metadata = {
+//     title: "Фабрика мебели ЗОВ | Корпусная мебель на заказ по индивидуальным размерам",
+//     description:
+//         "Кухонные гарнитуры и шкафы купе от белорусской фабрики ЗОВ. Изготовление корпусной мебели на заказ из материалов высокого качества.",
+// };
+
+
 
 export default () => {
+    const { currentFavoritesProducts } = favoritesProducts();
+
+    // Решаем ошибку гидратации
+    const [domLoaded, setDomLoaded] = useState(false)
+    useEffect(() => {
+        setDomLoaded(true)
+    }, [])
+
     return (
-        <div>123</div>
-    )
+        domLoaded && (
+            <>
+                <BreadCrumbs breadCrumbs={breadCrumbs} />
+                <ProductList products={currentFavoritesProducts} />
+            </>
+        )
+
+    );
 }
